ec0c3f45151dc4fd573a35d3562a790fd61c3885
Nico <nicolo.zanetti93@gmail.com>
2025-03-05 22:49:44 +0000

    cleaning up

--- backend/src/Greenfolio.API.Web/obj/Debug/net8.0/Greenfolio.API.Web.AssemblyInfo.cs
+++ backend/src/Greenfolio.API.Web/obj/Debug/net8.0/Greenfolio.API.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Greenfolio.API.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34acf6fd8a98cf81d89a7e51dc268f1495321fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2da8dee4ec9b9e46da6fea45acf5271bcafcd2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Greenfolio.API.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Greenfolio.API.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
